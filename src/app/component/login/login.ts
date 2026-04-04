01fbf07383d2c42364052d25267f697822805e01
pdudette151 <karim.sabrina@gmail.com>
2026-04-03 22:53:05 -0400

    add sign up and sign in with email and password

--- src/app/component/login/login.ts
+++ src/app/component/login/login.ts
@@ -1,14 +1,16 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Auth } from '../../services/auth';
-
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
+
 export class Login {
   public auth = inject(Auth);
-
+  email = signal('');
+  password = signal('');
 }
--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -1,5 +1,5 @@
 import { Injectable, inject } from '@angular/core';
-import { Auth as FirebaseAuth, GoogleAuthProvider, signInWithPopup, signOut, user } from '@angular/fire/auth';
+import { Auth as FirebaseAuth, GoogleAuthProvider, signInWithPopup, signOut, user, createUserWithEmailAndPassword, signInWithEmailAndPassword } from '@angular/fire/auth';
 import { Firestore, collection, collectionData, doc, setDoc } from '@angular/fire/firestore';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { Observable } from 'rxjs';
@@ -31,6 +31,28 @@ export class Auth {
       }, { merge: true });
     } catch (error) {
       console.error('Error signing in with Google:', error);
+    }
+  }
+
+  async createSignInWithEmail(email: string, password: string) {
+    try {
+      const result = await createUserWithEmailAndPassword(this.auth, email, password);
+      await setDoc(doc(this.firestore, 'users', result.user.uid), {
+        uid: result.user.uid,
+        displayName: result.user.displayName,
+        email: result.user.email,
+        photoURL: result.user.photoURL,
+      }, { merge: true });
+    } catch (error) {
+      console.error('Error creating user with email:', error);
+    }
+  }
+
+  async signInExistingUserWithEmail(email: string, password: string) {
+    try {
+      await signInWithEmailAndPassword(this.auth, email, password);
+    } catch (error) {
+      console.error('Error signing in with email:', error);
     }
   }
 
